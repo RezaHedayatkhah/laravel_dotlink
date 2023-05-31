7c0ed9df153947cb0df75405c3b70b3af1713482
Reza Hedayatkhah <rezahedayat80@gmail.com>
2023-05-31 17:25:00 +0330

    email verification bug 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('تاییدیه آدرس ایمیل')
+                ->line('روی دکمه زیر کلیک کنید')
+                ->action('تایید ایمیل', $url);
+        });    }
 }
